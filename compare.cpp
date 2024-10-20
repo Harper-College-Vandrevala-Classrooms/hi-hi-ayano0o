bc03f994d98925b7dad9dacdb6bff9af6c751adc
ayano0o <acarver0514@gmail.com>
2024-10-20 13:12:30 -0500

    allowed the program to skip spaces and added a max character compare limit

--- compare.cpp
+++ compare.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-   int strcmp_case_insensitive(const char *str1, const char *str2);
-
    void run_test_case() {
  
      const char *test_cases[][2]= {
@@ -18,8 +16,11 @@ using namespace std;
        {"test",""},
        {"Hold","Hald"},
        {"Big1","B1g"},
-       {"E242","D243"}
-  
+       {"E242","D243"},
+       {"Hello world", "HELLO WORLD"},  
+       {"Little ceasars", "LITTLECEASARS"},   
+       {"A C D C", "acdc"},              
+       {"ACDC", "a c d c"}   
    };
    
    int num_cases = sizeof(test_cases) / sizeof (test_cases[0]);
@@ -27,20 +28,27 @@ using namespace std;
    for (int i=0; i < num_cases; i++) {
        const char *str1 = test_cases[i][0];
        const char *str2 = test_cases[i][1];
-       int result = strcmp_case_insensitive(str1,str2);
-
-   if (result == 0) {
-      cout << "The comparison of "<< str1 <<" equals "<< str2 <<" the value returned is 0." << endl;;
-   }  else if ( result < 0) {
-      cout << "The comparison of "<< str1 <<" is less than "<< str2 <<" the value returned is -1." << endl;
-   }  else {
-      cout << "The comparison of "<< str1 <<" is greater than "<< str2 <<" the value returned is 1." << endl;
-   }
     
-  }
-  
-}
+      int result = strcmp_case_insensitive(str1,str2);
+      if (result == 0) {
+           cout << "The comparison of "<< str1 <<" equals "<< str2 <<" the value returned is 0." << endl;;
+      }  else if ( result < 0) {
+           cout << "The comparison of "<< str1 <<" is less than "<< str2 <<" the value returned is -1." << endl;
+      }  else {
+           cout << "The comparison of "<< str1 <<" is greater than "<< str2 <<" the value returned is 1." << endl;
+      }
+    
+     result = strcmp_case_insensitive(str1, str2, true, 4);
+     if (result == 0) {
+            cout << "The comparison of " << str1 << " equals " << str2 << " (spaces skipped, 4 compared char), returned 0." << endl;
+        } else if (result < 0) {
+            cout << "The comparison of " << str1 << " is less than " << str2 << " (spaces skipped, 4 compared char), returned -1." << endl;
+        } else {
+            cout << "The comparison of " << str1 << " is greater than " << str2 << " (spaces skipped, 4 compared char), returned 1." << endl;
+        }
+    }
    
+}
 
 int main() {
   run_test_case();
--- compare.hpp
+++ compare.hpp
@@ -3,15 +3,32 @@
 
 using namespace std;
 
-int strcmp_case_insensitive(const char *str1, const char *str2) {
+int strcmp_case_insensitive(const char *str1, const char *str2, bool skipSpaces = false, int maxChar = -1) {
+  int compareChar = 0;
+  
    while (*str1 && *str2) {
+     
+     if (skipSpaces) {
+        while (*str1==' ')str1++;
+        while (*str2==' ')str2++;
+     }
+     
      if (tolower(*str1) != tolower(*str2)) {
        return (tolower(*str1) < tolower(*str2)) ? -1 : 1;
      }
      str1++;
      str2++;
+     compareChar++;
 
  }
+ 
+    if (maxChar > 0 && compareChar >= maxChar) {
+        return 0;
+    }
+
+    if ((maxChar == -1 || compareChar < maxChar) && (*str1 || *str2)) {
+        return (*str1 == '\0') ? -1 : 1;
+    }
  
  if (*str1 == '\0' && *str2 == '\0') {
     return 0;
